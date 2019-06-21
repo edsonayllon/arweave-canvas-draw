f765bad9b8737d49ac88a25df04e950d77acbe97
eayllon1 <eayllon1@gmail.com>
2019-06-21 14:41:50 -0400

    Add App

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { SketchField, Tools } from 'react-sketch';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={{fontSize: 20}}>
-          Canvas Collection
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span style={{ color: 'white', fontSize: 15, borderSize: 1, borderColor: '#dff0ea', backgroundColor: '#574f7d', padding: 10, paddingLeft: 25, paddingRight: 25, borderRadius: 5}}>
-            Save
+  const [wallet, setWallet] = useState({});
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleFileUpload = (e) => {
+    const filereader = new FileReader();
+
+    filereader.addEventListener('loadend', async e => {
+      try {
+        const json = JSON.parse(e.target.result);
+        await setWallet(json);
+        await setAuthenticated(true);
+      } catch (err) {
+        alert(
+          'Something want wrong, make sure you have uploaded the correct file'
+        );
+        console.log(err);
+      }
+    });
+
+    filereader.readAsText(e.target.files[0]);
+  };
+
+  const handleLogout = () => {
+    setWallet({});
+    setAuthenticated(false);
+  }
+
+  if (authenticated) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div style={{display:'flex', flexDirection: 'row'}}>
+            <span style={{ display: 'flex', borderColor: '#574f7d', borderWidth: '1px', borderStyle: 'solid', height: 50, width: 75, borderRadius: 30, justifyContent: 'center', marginRight: 20 }}>
+              <img src="https://image.flaticon.com/icons/svg/214/214275.svg" alt="Credits to Pixel Buddha" hieght="30" width="30" />
+            </span>
+            <h1 style={{ fontSize: 20 }}>
+              Canvas Draw
+            </h1>
+          </div>
+          
+          <a
+            href="#"
+            onClick={handleLogout}
+          >
+            <span style={{ color: 'white', fontSize: 15, borderSize: 1, borderColor: '#dff0ea', backgroundColor: '#574f7d', padding: 10, paddingLeft: 25, paddingRight: 25, borderRadius: 5 }}>
+              Logout
           </span>
-        </a>
-      </header>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <a href="#">
-          <span style={{ display: 'flex', borderColor: '#95adbe', borderWidth: '1px', borderStyle: 'solid', height: 50, width: 75, borderRadius: 30, justifyContent: 'center', margin: 20 }}>
-            <img src="https://image.flaticon.com/icons/svg/214/214275.svg" hieght="30" width="30" />
-          </span>
-        </a>
-        
-        <SketchField
-          width='1024px'
-          height='400px'
-          tool={Tools.Pencil}
-          lineColor='black'
-          lineWidth={3}
-        />
-        
+          </a>
+        </header>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <SketchField
+            width='1024px'
+            height='580px'
+            tool={Tools.Pencil}
+            lineColor='black'
+            lineWidth={3}
+          />
+
+        </div>
       </div>
-    </div>
-  );
+    )
+  } else {
+    return (
+      <div className="App">
+        <div style={{ backgroundColor: '#4f3a65', color: '#dff0ea', flex: 1, height: '100%', justifyContent: 'center', display: 'flex', flexDirection: 'column'}}>
+          <h1>Login with your AR Wallet</h1>
+          <input type="file" onChange={handleFileUpload} style={{ borderStyle: 'dashed', padding: 30, margin: 50, borderWidth: 5, borderColor: 'rgba(255, 255, 255, 0.25)', display: 'flex', backgroundColor: 'rgba(255, 255, 255, 0.05)' }} />
+        </div>
+      </div>
+    )
+  } 
 }
 
 export default App;
